9bff37c5417dd54ccabb0b446ec3f9b851db753e
Oliver Oehlenberg <oliver@oehlenberg.de>
2022-02-13 13:10:44 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -3,46 +3,41 @@ import * as jose from 'jose';
 import * as timestamp from 'unix-timestamp';
 
 async function run() {
-    const secret: string = core.getInput('secret');
-    const issuer: string = core.getInput('issuer');
-    const audience: string = core.getInput('audience');
-    const validDays: number = +core.getInput('validDays') || 14;
-    const offline: string = core.getInput('offline') || 'NO';
-    const contact: string = core.getInput('contact');
-    const devenv: string = core.getInput('devenv') || 'NO';    
+  const secret: string = core.getInput('secret');
+  const issuer: string = core.getInput('issuer');
+  const audience: string = core.getInput('audience');
+  const validDays: number = +core.getInput('validDays') || 14;
+  const offline: string = core.getInput('offline') || 'NO';
+  const contact: string = core.getInput('contact');
+  const devenv: string = core.getInput('devenv') || 'NO';
 
+  try {
+    var payloadData = {
+      iss: issuer,
+      aud: audience,
+      sub: contact,
+      off: offline,
+      dev: devenv
+    };
 
-    try {
-        
-        var payloadData = {
-            "iss": issuer,
-            "aud": audience,
-            "sub": contact,
-            "off": offline,
-            "dev": devenv
-          };
+    const expDate = new Date();
+    expDate.setDate(expDate.getDate() + validDays);
+    let utf8Encode = new TextEncoder();
 
-        const expDate = new Date();
-        expDate.setDate(expDate.getDate() + validDays);
-        let utf8Encode = new TextEncoder();
+    const jwt = await new jose.SignJWT(payloadData)
+      .setProtectedHeader({alg: 'HS256'})
+      .setIssuedAt()
+      .setExpirationTime(timestamp.fromDate(expDate))
+      .sign(utf8Encode.encode(secret));
 
-        const jwt = await new jose.SignJWT(payloadData)
-          .setProtectedHeader({alg: 'HS256'})
-          .setIssuedAt()
-          .setExpirationTime(timestamp.fromDate(expDate))
-          .sign(utf8Encode.encode(secret))
+    console.log(jwt);
+  } catch (err) {
+    console.error(
+      `⚠️ An error happened executing JWT signing...`,
+      err?.message ?? err
+    );
 
-          console.log(jwt);
-    
-    } catch (err) {
-
-        console.error(
-            `⚠️ An error happened executing JWT signing...`,
-            err?.message ?? err
-          );
-
-        core.setFailed(err.message);
-        process.abort();
-
-    }
+    core.setFailed(err.message);
+    process.abort();
   }
+}
