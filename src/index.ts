9da6456aa7f899784c35d51cf8f4bbf6c72ae678
Oliver Oehlenberg <oliver@oehlenberg.de>
2022-02-13 12:34:22 +0100

    First Test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core';
+import * as jose from 'jose';
+import * as timestamp from 'unix-timestamp';
 
 async function run() {
     const secret: string = core.getInput('secret');
@@ -8,9 +10,39 @@ async function run() {
     const offline: string = core.getInput('offline') || 'NO';
     const contact: string = core.getInput('contact');
     const devenv: string = core.getInput('devenv') || 'NO';    
+
+
     try {
+        
+        var payloadData = {
+            "iss": issuer,
+            "aud": audience,
+            "sub": contact,
+            "off": offline,
+            "dev": devenv
+          };
+
+        const expDate = new Date();
+        expDate.setDate(expDate.getDate() + validDays);
+        let utf8Encode = new TextEncoder();
+
+        const jwt = await new jose.SignJWT(payloadData)
+          .setProtectedHeader({alg: 'HS256'})
+          .setIssuedAt()
+          .setExpirationTime(timestamp.fromDate(expDate))
+          .sign(utf8Encode.encode(secret))
+
+          console.log(jwt);
     
     } catch (err) {
-     
+
+        console.error(
+            `⚠️ An error happened executing JWT signing...`,
+            err?.message ?? err
+          );
+
+        core.setFailed(err.message);
+        process.abort();
+
     }
   }
